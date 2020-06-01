0394ecc8dc25f465c831d5cd579ee6aa4e5429f3
Gen Yap Feng Yuan <58755531+TheKinng96@users.noreply.github.com>
2020-06-01 14:54:15 +0900

    finished the basic codes

--- venv/main.py
+++ venv/main.py
@@ -4,4 +4,18 @@
 
 from random import randint
 
-answer
+answer = randint(1,10)
+print(answer)
+
+while True:
+    try:
+        guess = int(input("tell me a num in betw 1~10:  "))
+        if 0 < guess < 11:
+            if guess == answer:
+                print("congrats")
+                break
+        else:
+            print("hey bro, 1 ~ 10 pls")
+    except ValueError:
+        print("please try again with a number")
+        continue
