890b9e49aa76e332177ddb5496e199016fbf12eb
Gen Yap Feng Yuan <58755531+TheKinng96@users.noreply.github.com>
2020-06-01 15:11:01 +0900

    added sys pack to the file and implemented a show higher or lower notice on the guesses, the num will be auto changed when the num 1 and num 2 are set

---
+++ main.py
@@ -0,0 +1,29 @@
+# get random num
+# get input
+# check answer
+
+from random import randint
+import sys
+
+num1 = sys.argv[1]
+num2 = sys.argv[2]
+
+answer = randint(int(num1), int(num2))
+print(answer)
+
+while True:
+    try:
+        guess = int(input(f"tell me a num in betw {num1}~{num2}:  "))
+        if (int(num1) - 1) < guess < (int(num2) + 1):
+            if guess < answer:
+                print("guess a higher number")
+            elif guess > answer:
+                print("your guess too high")
+            else:
+                print("congrats")
+                break
+        else:
+            print(f"hey bro, {num1} ~ {num2} pls")
+    except ValueError:
+        print("please try again with a number")
+        continue
