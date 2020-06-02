2490534036a7e452b7b4866ec75ebda71bfa6aba
Gen Yap Feng Yuan <58755531+TheKinng96@users.noreply.github.com>
2020-06-02 14:43:09 +0900

    added unit test files.

--- main.py
+++ main.py
@@ -5,24 +5,30 @@
 from random import randint
 import sys
 
-num1 = sys.argv[1]
-num2 = sys.argv[2]
+num1 = sys.argv(1)
+num2 = sys.argv(2)
 
-answer = randint(int(num1), int(num2))
-
-while True:
-    try:
-        guess = int(input(f"tell me a num in betw {num1}~{num2}:  "))
-        if (int(num1) - 1) < guess < (int(num2) + 1):
-            if guess < answer:
-                print("guess a higher number")
-            elif guess > answer:
-                print("your guess too high")
-            else:
-                print("congrats")
-                break
+def run_guess(guess, answer):
+    if (int(num1) - 1) < guess < (int(num2) + 1):
+        if guess < answer:
+            print("guess a higher number")
+        elif guess > answer:
+            print("your guess too high")
         else:
-            print(f"hey bro, {num1} ~ {num2} pls")
-    except ValueError:
-        print("please try again with a number")
-        continue
+            print("congrats")
+            return True
+    else:
+        print(f"hey bro, {num1} ~ {num2} pls")
+        return False
+
+if __name__ == '__main__':
+    answer = randint(int(num1), int(num2))
+
+    while True:
+        try:
+            guess = int(input(f"tell me a num in betw {num1}~{num2}:  "))
+            if (run_guess(guess, answer)):
+                break
+        except ValueError:
+            print("please try again with a number")
+            continue
